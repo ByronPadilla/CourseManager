164cbd1f9bd8c52d0fcbe8b5ffbe3008513705fe
Byron Padilla <byronpadilla@byrons-mbp.lan>
2017-07-20 23:06:54 -0700

    Initial commit

---
+++ Course.py
@@ -0,0 +1,12 @@
+class Course:
+    """
+    Python class designed to hold information on Students. Interacts with Students by creating Stats based on Student
+    information.
+
+    Course has the following properities:
+
+    Attributes:
+        student_Dict: Dictionary of Students in the Course.
+        count_Student: The Number of Studnets in the Course.
+
+    """
---
+++ Main.py
@@ -0,0 +1,10 @@
+import CourseManager
+
+def main():
+    myCourseManager = CourseManager.courseManager(1)
+
+    myCourseManager.run()
+
+
+if __name__ =="__main__":
+    main()
